0ed0e5d72c8393947032191356534ea0ccc1b38f
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-07-30 12:19:14 +0530

    feat: add authentication middleware and error handling; refactor JWT token generation

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 import env from "../config/env";
 
-const generateToken = (userId: string, option: jwt.SignOptions = { }) => {
-  return jwt.sign({ userId }, env.JWT, { expiresIn: "24h", ...option});
+const generateToken = (payload: Record<string, unknown>, option: jwt.SignOptions = { }) => {
+  return jwt.sign(payload, env.JWT, { expiresIn: "24h", ...option});
 }
 
 const verifyToken = (token: string) => {
