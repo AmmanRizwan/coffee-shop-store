9e22a5853001ccb72a22467b7e93e082e738e7d0
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-08-11 13:41:17 +0530

    feat(nav-bar): add WebNavBar component with Coffee icon

---
+++ frontend/src/components/nav-bar/index.tsx
@@ -0,0 +1,16 @@
+import { Coffee } from "lucide-react"
+
+export const WebNavBar = () => {
+    return (
+        <div className="flex justify-between items-center p-3 bg-blue-300">
+            <div className="border p-2 rounded-full border-black"><Coffee /></div>
+            <div></div>
+        </div>
+    )
+}
+
+export const MovBar = () => {
+    return (
+        <div></div>
+    )
+}
